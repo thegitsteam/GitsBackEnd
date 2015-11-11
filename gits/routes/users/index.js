bcd1292fbfd9543f3176f5f8d68c871548a8042f
enriquesleon <enriquesleon>
2015-11-11 10:07:33 -0800

    fixed a couple of lines, added mongoose as a dependency

--- gits/routes/users/index.js
+++ gits/routes/users/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/citycrew',require('./citycrew'));
-router.use('/citycrew',require('./law'));
-router.use('/citycrew',require('./admin'));
+router.use('/law',require('./law'));
+router.use('/admin',require('./admin'));
 module.exports = router;
